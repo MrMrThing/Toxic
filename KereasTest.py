46dcd916491d4c58ac4fbede2ab2f8ff66f87401
MrMrThing <rasmus031299@gmail.com>
2022-03-10 13:14:56 +0100

    Update

--- KereasTest.py
+++ KereasTest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
 data = pd.read_excel('C:/Users/rasmu/Desktop/Train.xlsx', names = ['id', 'sentence', 'toxic', 'severe_toxic', 'obscene' , 'threat' , 'insult', 'identity_hate'])
@@ -14,7 +15,7 @@ sentences = df['sentence'].values
 y = df['toxic'].values
 
 sentences_train, sentences_test, y_train, y_test = train_test_split(
-    sentences,y,test_size=0.10,random_state=1000)
+    sentences,y,test_size=0.20,random_state=1000)
 
 
 vectorizer = CountVectorizer()
@@ -24,8 +25,10 @@ X_test  = vectorizer.transform(sentences_test)
 X_train
 
 
-classifier = LogisticRegression()
+classifier = DecisionTreeClassifier()
 classifier.fit(X_train, y_train)
 score = classifier.score(X_test, y_test)
+
+
 
 print("Accuracy:", score)
