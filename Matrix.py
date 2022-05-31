8932b1816f1b2ae9a7a4c7e5648e7b584a6554a6
MrMrThing <rasmus031299@gmail.com>
2022-05-31 22:15:57 +0200

    Cleaning and comments

--- Matrix.py
+++ Matrix.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 import pandas as pd
 import time
 from sklearn.model_selection import train_test_split
@@ -7,61 +6,74 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import load_iris
-from sklearn import tree
 from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
-import re
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 
 
-
+#Importing data
 data = pd.read_excel('C:/Users/rasmu/Desktop/Train.xlsx', names = ['id', 'sentence', 'toxic', 'severe_toxic', 'obscene' , 'threat' , 'insult', 'identity_hate'])
 test = pd.read_csv('C:/Users/rasmu/Desktop/Test.csv', names = ['id', 'comments'])
 result = pd.read_csv('C:/Users/rasmu/Desktop/test_labels.csv', names = ['id','toxic', 'severe_toxic', 'obscene' , 'threat' , 'insult', 'identity_hate'])
 
+#Making Dataframe's
 df = pd.DataFrame(data)
 df_result = pd.DataFrame(result)
 df_test = pd.DataFrame(test)
 
+#Selecting the sentences from the dataframe
 sentences = df['sentence'].values
 results = df_result['toxic'].values
 test_sentences = df_test['comments'].values
 
+#Here we define what label we want to work with
 y = df['toxic'].values
 
+#Info on the dataframe
 df_result.head()
-
 df_test.head()
 
-
+#Splitting the data, test_size is how big in % the split is
 sentences_train, sentences_test, y_train, y_test, = train_test_split(
     sentences,y,test_size=0.20,random_state=42)
 
 
+#Test sentence for a quick prediction
 test_sentence = ['the quick brown fox jumps over the lazy dog']
 
+#Making an Count Vectorizer Template
+#And fitting it to the sentences
 vectorizer = CountVectorizer(lowercase=False)
 vectorizer.fit(sentences_train)
+
+#Transforming or training and test sets
 X_train = vectorizer.transform(sentences_train)
 X_test  = vectorizer.transform(sentences_test)
 X_train
 
+
+
+
+#Transforming the test sentence
+Test = vectorizer.transform(test_sentence)
 X_test_sentences = vectorizer.transform(test_sentences)
 
-Test = vectorizer.transform(test_sentence)
-
+#Checking the transforming was right
 print(Test)
 
+#setting an timer to see how long the training takes
 start = time.time()
 
-classifier = KNeighborsClassifier()
+#Defining the used classifier, this can be changed
+#And fitting that classifier on our training set
+classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 
+#Making the prediction on the test sentence
+#And using the test split to give us a accuracy score
 prediction = classifier.predict(Test)
 score = classifier.score(X_test, y_test)
 
+#Printing infomation to the user, and stopping the timer
 print("Give me a sentence")
 
 print("Accuracy:", score)
@@ -70,33 +82,44 @@ print("Prediction", prediction)
 stop = time.time()
 print(f"Training time: {stop - start}s")
 
-#np.isnan(df_test.any()) #and gets False
-#np.isfinite(df_result.all()) #and gets True
+#The rest is code to make the matrix
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import plot_confusion_matrix
-
-
-
+#Length of all the sentences
 length = int(len(test_sentences))
 
+#Going though the results and making them and int
+#Some of them are strings
 for i in range(1, length):
     results[i] = int(results[i])
     
 
-
+#Variable holders for later
 TP = 0
 TN = 0
 FP = 0
 FN = 0
 
+#Information to the user
 print(length)
 
+#Using k as an increment for showing progress
+k = 0.05
 
-
+#The loop for every sentence
+#We start at one becouse the comments starts at the second row
 for i in range(1, length-1):
+
+    
+    #This is just to show the progress of the whole process
+    if i >= k * length:
+        print(k*100, "%")
+        k = k + 0.05
+
+    #Making a prediction on a sentence and setting it to be and int
+    #We start with +1 becouse the test set, starts one row behind the label set
     prediction = int(classifier.predict(X_test_sentences[i+1]))
 
+    #Going though the scenarios and counting them 
     if prediction == 1 and results[i] == -1 or prediction == 1 and results[i] == 1:
         TP = TP + 1
 
@@ -109,12 +132,14 @@ for i in range(1, length-1):
     elif prediction == 0 and results[i] == -1 or prediction == 0 and results[i] == 1:
         FP = FP + 1
 
+    #If none of the above went through something went wrong
+    #So here is information on what row, and what the variables where
     else:
         print(i)
         print(prediction, results[i])
         break
 
-
+#Ending by giving the information to the user
 print("TP", TP/length)
 print("TN", TN/length)
 print("FP", FP/length)
--- Toxic.py
+++ Toxic.py
@@ -7,27 +7,23 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import load_iris
-from sklearn import tree
-from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
-import re
-from sklearn.metrics import classification_report, confusion_matrix
 
+#Importing data
 data = pd.read_excel('C:/Users/rasmu/Desktop/Train.xlsx', names = ['id', 'sentence', 'toxic', 'severe_toxic', 'obscene' , 'threat' , 'insult', 'identity_hate'])
+test = pd.read_csv('C:/Users/rasmu/Desktop/Test.csv')
 
-test = pd.read_excel('C:/Users/rasmu/Desktop/Test.xlsx')
-
+#Making Dataframe's
 df = pd.DataFrame(data)
-
 df_test = pd.DataFrame(test)
   
+#Info on the dataframe
 df.info()
 
-
+#Selecting the sentences from the dataframe
 sentences = df['sentence'].values
 
+#Little function for erasing special characters from sentences  
 """
-
 for i in range(len(sentences)):
 
     sentences[i] = re.sub('\W+',' ', sentences[i] )
@@ -41,33 +37,47 @@ for i in range(len(sentences)):
 print("100%")
 """
 
+#Here we define what label we want to work with
 y = df['toxic'].values
 
+#Splitting the data, test_size is how big in % the split is
 sentences_train, sentences_test, y_train, y_test = train_test_split(
     sentences,y,test_size=0.20,random_state=42)
 
 
-
+#Test sentence for a quick prediction
 test_sentence = ['the quick brown fox jumps over the lazy dog']
 
+#Making an Count Vectorizer Template
+#And fitting it to the sentences 
 vectorizer = CountVectorizer(lowercase=False)
 vectorizer.fit(sentences_train)
+
+#Transforming or training and test sets
 X_train = vectorizer.transform(sentences_train)
 X_test  = vectorizer.transform(sentences_test)
 X_train
 
+#Transforming the test sentence
 Test = vectorizer.transform(test_sentence)
 
+#Checking the transforming was right
 print(Test)
 
+#setting an timer to see how long the training takes
 start = time.time()
 
+#Defining the used classifier, this can be changed
+#And fitting that classifier on our training set
 classifier = KNeighborsClassifier()
 classifier.fit(X_train, y_train)
 
+#Making the prediction on the test sentence
+#And using the test split to give us a accuracy score
 prediction = classifier.predict(Test)
 score = classifier.score(X_test, y_test)
 
+#Printing infomation to the user, and stopping the timer
 print("Give me a sentence")
 
 print("Accuracy:", score)
@@ -76,40 +86,45 @@ print("Prediction", prediction)
 stop = time.time()
 print(f"Training time: {stop - start}s")
 
-"""
-iris = load_iris()
-sentences, y = iris.data, iris.target
-tree.plot_tree(classifier)
-"""
+#function to predict a full test set
 def predictDataSet(DataFrame):
 
+    #Using k as an increment for showing progress
     k = 0.1
+
+    #making an dataframe that will be used later
     df_predictions = pd.DataFrame(columns = ['id', 'statement', 'prediction'])
     
+    #Length of the dataframe, and making an array to hold the predictions
     length = int(len(DataFrame))
     print(length)
     allPredictions = []
 
+    #The loop for every datapoint in the dataframe
     for i in range(length):
 
+        #Taking a sentence from the dataframe
         test_sentence = DataFrame.iloc[i]
 
+        #Transforming and predicting the sentence
         Test = vectorizer.transform(test_sentence)
         prediction = classifier.predict(Test)
-        print(i)
 
+        #Inserting the prediction into the array
         allPredictions.append(prediction)
-
-        #df_predictions = df.append(DataFrame.iloc[i], DataFrame.iloc[i], prediction)
-
         test_sentence = NULL
+
+        #This is just to show the progress of the whole process
         if i >= k * length:
             print(k*100, "%")
             k = k + 0.1
 
+    #When the loop is done, we take all the predictions and setting them into the dataframe
+    #Then exporting that dataframe out
     DataFrame.insert(len(DataFrame.columns), 'predictions', allPredictions)
     print(DataFrame)
     DataFrame.to_excel('pandas_to_excel.xlsx', sheet_name='new_sheet_name')
     return NULL
 
+#Calling the functing, needs a dataframe to predict
 predictDataSet(df_test)
